3b9348e78584fe50466fe14cc3244f8d64a34fb5
Tony Shen <fengzhizi715@126.com>
2021-10-30 00:19:13 +0800

    add Main

---
+++ server/src/main/kotlin/com/github/websocket/Main.kt
@@ -0,0 +1,14 @@
+package com.github.websocket
+
+/**
+ *
+ * @FileName:
+ *          com.github.websocket.Main
+ * @author: Tony Shen
+ * @date: 2021/10/30 12:16 上午
+ * @version: V1.0 <描述当前版本功能>
+ */
+fun main() {
+
+    WebSocketServer.execute()
+}
